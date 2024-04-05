261dd9e3d752ef3faf6c6d225aa326545802d20a
ojagao <ahiroto1234@gmail.com>
2024-04-05 17:22:04 +0900

    Fix build error

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index'
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _, next) => {
   document.title = (to.meta.title as string) || '世界旅行'
   next()
 })
+
 
 createApp(App).use(router).mount('#app')
